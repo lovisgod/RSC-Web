ebbf66b2c3fc0fd0d849b33f2dbfba79a5b4b2dd
Justin <cjustinobi@gmail.com>
2026-07-02 19:36:55 +0100

    feat: add generic image upload endpoint

--- apps/api/src/media/media.module.ts
+++ apps/api/src/media/media.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 
+import { AuthModule } from "../auth/auth.module";
 import { MediaController } from "./media.controller";
 import { MediaService } from "./media.service";
 
 @Module({
+  imports: [AuthModule],
   controllers: [MediaController],
   providers: [MediaService],
   exports: [MediaService],
