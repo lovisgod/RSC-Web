c0e8965241b32f08e91bfaf9ec3cd0f93a04bcef
Justin <cjustinobi@gmail.com>
2026-07-09 21:00:13 +0100

    fix(outlets): add AuthModule to imports and export PAYMENT_ADAPTER from PaymentsModule

--- apps/api/src/outlets/outlets.module.ts
+++ apps/api/src/outlets/outlets.module.ts
@@ -2,13 +2,14 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { Customer } from "../auth/customer.entity";
+import { AuthModule } from "../auth/auth.module";
 import { PaymentsModule } from "../payments/payments.module";
 import { Outlet } from "./outlet.entity";
 import { OutletsController } from "./outlets.controller";
 import { OutletsService } from "./outlets.service";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Outlet, Customer]), PaymentsModule],
+  imports: [TypeOrmModule.forFeature([Outlet, Customer]), AuthModule, PaymentsModule],
   controllers: [OutletsController],
   providers: [OutletsService],
   exports: [OutletsService, TypeOrmModule],
