577543a633261e5970ad703ba618fb39d1b952bd
Justin <cjustinobi@gmail.com>
2026-07-12 13:30:04 +0100

    import auth module in FinanceModule

--- apps/api/src/finance/finance.module.ts
+++ apps/api/src/finance/finance.module.ts
@@ -1,13 +1,14 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
+import { AuthModule } from "../auth/auth.module";
 import { Outlet } from "../outlets/outlet.entity";
 import { FinanceController } from "./finance.controller";
 import { FinanceService } from "./finance.service";
 import { OutletSettlementApproval } from "./outlet-settlement-approval.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Outlet, OutletSettlementApproval])],
+  imports: [AuthModule, TypeOrmModule.forFeature([Outlet, OutletSettlementApproval])],
   controllers: [FinanceController],
   providers: [FinanceService],
 })
